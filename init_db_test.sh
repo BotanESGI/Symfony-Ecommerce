7c0b65215eb3f33dafe228294cb36706591f1be7
Botan YILMAZ <byilmaz1@myges.fr>
2025-02-03 19:24:44 +0100

    -Adpate cmd perso for the first installation and reinitialisation + change docker for create and migrate bdd app_test, fix phpunit test

---
+++ init_db_test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Attendre que la bdd est prête
+until nc -z database 5432; do
+  echo "Waiting for the database..."
+  sleep 2
+done
+
+# Créer la base de données pour l'environnement de test et lance les migrations
+php bin/console doctrine:database:create --env=test
+php bin/console doctrine:migrations:migrate --env=test
